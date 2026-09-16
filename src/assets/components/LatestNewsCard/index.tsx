9aaf142bc6e0e54e40ad8618528bfaf89ead57b3
Zakhar Salov <eaglechannel4@gmail.com>
2026-09-16 19:05:44 +0700

    create subscribe

---
+++ src/assets/components/LatestNewsCard/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { Icon } from "../Icon";
+
+import styles from "./index.module.scss";
+
+function LatestNewsCard() {
+  return (
+    <>
+      <div className={styles.card}>
+        <h3 className={styles.cardTitle}>
+          Get Your First Month of Ubisoft+ Classics for $1
+        </h3>
+        <img className={styles.cardImg} src="https://placehold.co/481x328" alt="Заглушка"></img>
+        <p className={styles.cardText}>
+          Play some of these beloved games without breaking the bank! From now until January 2, new subscribers can get their first month of Ubisoft+ Classics for just $1 USD.
+        </p>
+        <Link className={styles.cardBtn} to="/news:slug">
+          <Icon className={styles.cardBtnSvg} name="document-mini-icon" size={24} />
+          Open the post
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default LatestNewsCard;
