4871d5cbbf782e73a2cd52a7be9524333c61c722
Zakhar Salov <eaglechannel4@gmail.com>
2026-09-16 13:07:43 +0700

    fix header commit

--- src/assets/components/MainNav/index.tsx
+++ src/assets/components/MainNav/index.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-<<<<<<< HEAD
 import styles from "./index.module.scss";
 
 function MainNav() {
@@ -26,17 +25,6 @@ function MainNav() {
           <NavLink className={styles.mainNavLink} to="/contact">Contact</NavLink>
         </li>
       </ul>
-=======
-function MainNav() {
-  return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/shop">Store</NavLink>
-      <NavLink to="/services">Services</NavLink>
-      <NavLink to="/news">News</NavLink>
-      <NavLink to="/support">Support</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
->>>>>>> 4280d38601138dbe5c4b1654018cec4465661ae5
     </nav>
   )
 }
